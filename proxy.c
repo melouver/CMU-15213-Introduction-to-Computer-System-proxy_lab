09fc81b3cce7cd7ce6dcad963d971b246af3c5c2
lyz <yuzheli@vip.qq.com>
2017-12-02 23:03:29 +0800

    adder ok proxy ok

--- proxy.c
+++ proxy.c
@@ -1,18 +1,87 @@
 #include <stdio.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
+void doit(int fd);
+
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+//static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("%s", user_agent_hdr);
-    
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(0);
+  }
+  int listenfd, connfd;
+  char hostname[MAXLINE], port[MAXLINE];
+  socklen_t clientlen;
+  struct sockaddr_storage clientaddr;
 
-
-
+  listenfd = Open_listenfd(argv[1]);
+  while (1) {
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA*)&clientaddr, &clientlen);
+    Getnameinfo((SA*)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    printf("Accepted connection from (%s, %s)\n", hostname, port);
+    doit(connfd);
+    Close(connfd);
     return 0;
+
+  }
+  return 0;
 }
+
+void doit(int clifd) {
+  rio_t toCli;
+  Rio_readinitb(&toCli, clifd);
+
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  if (!Rio_readlineb(&toCli, buf,MAXLINE)) {
+    return;
+  }
+  printf("REQUEST: %s\n", buf);
+  sscanf(buf, "%s %s %s", method, uri, version);
+
+  if (strcasecmp(method, "GET")) {
+    return;
+  }
+ 
+  char Request[MAXLINE], host[MAXLINE], port[MAXLINE];
+  
+  char *fileP = strstr(uri+7, "/");
+  printf("uri: %s file: %s\n", uri, fileP);
+  sprintf(host, "127.0.0.1");
+  sprintf(port, "10000");
+  
+  sprintf(Request, "GET /cgi-bin/adder?1&2 HTTP/1.0\r\n\r\n");
+  int clientfd;
+  
+  if ((clientfd = open_clientfd(host, port)) < 0) {
+    fprintf(stderr, "Error");
+    exit(-1);
+  }
+  rio_t toServ;
+  Rio_readinitb(&toServ, clientfd);
+  
+  if (rio_writen(clientfd, Request, strlen(Request)) < 0) {
+    fprintf(stderr, "Error writing to server\n");
+  }
+  char tmp[MAXLINE] = {0};
+  while (rio_readlineb(&toServ, buf, MAXLINE) > 0) {
+    sprintf(tmp, "%s%s", tmp, buf);
+    printf("%s", buf);
+  }
+  rio_writen(clifd, tmp, strlen(tmp));
+  
+  
+  
+  
+  
+  
+  
+}
+
